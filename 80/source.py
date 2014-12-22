87a84d5241714459dc829a7b38bf9212cffc69db
Smiks <sandi.mikus@gmail.com>
2014-12-22 14:59:00 +0100

    Added updated solution for problem 80

--- 80/source.py
+++ 80/source.py
@@ -5,13 +5,4 @@ squares = {i**2 for i in range(1,100)}
 precision = 100
 getcontext().prec = precision + 3
 
-sum = 0
-for i in range(1,101):
-    if i not in squares:
-        tmp = Decimal(i).sqrt()
-        tmp2 = str(tmp).replace(".","")
-        tmp2 = tmp2[0:100]
-        for j in tmp2:
-            sum += int(j)
-
-print("Result: ", sum)
+print ("Result: ", sum ( sum(int(j) for j in ((str(Decimal(i).sqrt())).replace(".",""))[0:100]) for i in range(1,101) if i not in squares) )
