f9a823cd3f0015e5957803d9e6fc84db3a028d29
Smiks <sandi.mikus@gmail.com>
2014-12-29 03:02:36 +0100

    Added upgraded solution for problem 26

--- 26/source.py
+++ 26/source.py
@@ -1,30 +1,23 @@
-from decimal import Decimal, getcontext
-getcontext().prec = 4000
+from collections import defaultdict
 
-def longestSubstring(a, d):
+def check(n):
+    repeats = defaultdict(int)
+    tmp = 10%n
+    repeats[tmp] = 1
+    len = 1
+    while True:
+        tmp = (tmp*10) % n
+        len += 1
+        repeats[tmp] += 1
+        if repeats[tmp] == 2:
+            break
+    return len
 
-    # converting to string and ignoring first non-decimal part 0.
-    a = str(a)
-    t = a[2:]
+maxLen = 0
+for i in range(1,1000):
+    tmp = check(i)
+    if tmp > maxLen:
+        maxLen = max(maxLen, tmp)
+        result = i
 
-    # start at d as d-1 decimals can repeat
-    for i in range(d, len(t)):
-        # checking for numbers like 0.1666666666 if 6 repeats...
-        t1 = t[d:d+1]
-        t2 = t[d+1:d+2]
-        if t1 == t2:
-            return 1
-
-        # checking for two parts
-        # if it repeats they will eventually become equal
-        p1 = t[0:i]
-        p2 = t[i:i*2]
-        if p1 == p2:
-            # because we start at d :: p1 and p2 will be concatenated p1p2 = p1p2
-            # length is half of p1p2 = p1
-            return len(p1)//2
-    return 0
-
-result = [ (longestSubstring(Decimal(1)/Decimal(i), i) , i)  for i in range(1, 1000) ]
-print("D:", max(result)[1])
-print("Length: ", max(result)[0])
+print("Result: ", result)
